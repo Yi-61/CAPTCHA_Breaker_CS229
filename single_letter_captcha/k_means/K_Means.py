6b55e517dd63ab566ef9d4b4783c3be974273f09
Yi-61 <33105174+Yi-61@users.noreply.github.com>
2017-12-14 22:43:01 -0800

    K-means

--- single_letter_captcha/k_means/K_Means.py
+++ single_letter_captcha/k_means/K_Means.py
@@ -55,31 +55,3 @@ for x in range(0,10):
     labelName = 'Cluster label:' + str(realLabels[trainSelectedLabel[x,]])
     plt.title(labelName)
     plt.show()
-
-
-# #plot
-# dim1 = 0
-# dim2 = 1 #dimensions of centroids that we will plot
-# h = .02 #for mesh
-# x_min, x_max = trainData[:, dim1].min() - 1, trainData[:, dim1].max() + 1
-# y_min, y_max = trainData[:, dim2].min() - 1, trainData[:, dim2].max() + 1
-# xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
-# 
-# #Obtain label for each point in mesh, using trained model
-# Z = kmeans.predict(np.c_[xx.ravel(),yy.ravel()])
-# 
-# #plot the result into a color plot
-# plt.figure(1)
-# plt.clf()
-# plt.imshow(Z, interpolation='nearest',extent=(xx.min(), xx.max(), yy.min(), yy.max()),
-#            aspect='auto', origin='lower')
-# plt.plot(trainData[0:dim1],trainData[:,dim2],'k.',markersize=2)
-# 
-# centroids = kmeans.cluster_centers_
-# plt.scatter(centroids[:,dim1],centroids[:,dim2],marker = 'x',s=169, linewidths=3,color='w', zorder=10)
-# plt.xlim(x_min, x_max)
-# plt.ylim(y_min, y_max)
-# plt.xticks(())
-# plt.yticks(())
-# plt.show()
-
